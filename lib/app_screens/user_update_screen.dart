4bedee3a3b0deb002eccff30f252b468f1bf5a4f
Siddharth Notani <s.notani@lancaster.ac.uk>
2019-01-29 16:56:39 +0000

    Added Title and logo

--- lib/app_screens/user_update_screen.dart
+++ lib/app_screens/user_update_screen.dart
@@ -26,19 +26,58 @@ class _user_update_screen extends State<user_update_screen> {
        padding: EdgeInsets.only(top: TOP_PADDING),
        child: ListView(
         children: <Widget>[
-          Center(
-            child: Text(
-                "Update Fridge Items",
-                textDirection: TextDirection.ltr,
-                style: TextStyle(
-                    fontSize: 50.0,
-                ),
+          Row(
+              children: <Widget>[
+           Expanded(
+       child: Padding(
+       padding: const EdgeInsets.only(top: 30.0, right: 50.0),
+       child: Text(
+         "Update Fridge Items",
+         textDirection: TextDirection.ltr,
+         style: TextStyle(
+           fontSize: 50.0,
+         ),
+       ),
+     ),
+
+       ),
+      Expanded(
+        child: add_icon_asset() ,
+      )
+
+         ],
+
             ),
           ),
+
         ]
        ),
      ),
     ),
+    );
+  }
+}
+
+//widget that holds the image and returns it in a container
+class add_icon_asset extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //creating an object that passes that image/logo as a parameter
+
+    AssetImage assetImage = AssetImage("images/add_icon.png");
+    //create an image object from the assetimage object and pass it as parameter
+    //resize the image with set heights and width
+
+    Image add_Icon_image = Image(
+      image: assetImage,
+      width: 300.0,
+      height: 300.0,
+    );
+
+    //the method will return the logo image as a container
+    // with in it will be a child that will hold the image
+    return Container(
+      child: add_icon_image,
     );
   }
 }

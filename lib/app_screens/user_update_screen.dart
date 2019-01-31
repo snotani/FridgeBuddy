f593dd9fab4650404961c9b73140f08be1137c75
Siddharth Notani <36894543+snotani@users.noreply.github.com>
2019-01-31 17:47:17 +0000

    Added appbar and link

--- lib/app_screens/user_update_screen.dart
+++ lib/app_screens/user_update_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'admin_login_screen.dart';
 //import 'package:cloud_firestore/cloud_firestore.dart';
-//import 'admin_login_screen.dart';
+
 
 class user_update_screen extends StatefulWidget {
   @override
@@ -9,61 +10,43 @@ class user_update_screen extends StatefulWidget {
 
 class _user_update_screen extends State<user_update_screen> {
 
-  var _formKey = GlobalKey<FormState>();
-  final TOP_PADDING = 40.0;
-  final LEFT_PADDING = 50.0;
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        //resizeToAvoidBottomPadding: false,
-        appBar: AppBar(
-        title: Text("Update Fridge Items"),
-    ),
-    body: Form(
-      key: _formKey,
-        child: Padding(
-          padding: EdgeInsets.only(top: TOP_PADDING),
-            child: ListView(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.only(top: 30.0, right: 50.0),
-                          child: Text(
-                              "Update Fridge Items",
-                              textDirection: TextDirection.ltr,
-                              style: TextStyle(
-                                fontSize: 50.0,
-                              ),
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: add_icon_asset(),
-                      ),
-                    ]
-                  ),
-              ]
+    return new MaterialApp(
+      title: 'User Update Screen',
+      debugShowCheckedModeBanner: false,
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Update Fridge Items"),
+          centerTitle: true,
+          elevation: 20.0,
+          actions:  <Widget> [
+            IconButton(
+              tooltip: 'Admin Login',
+              icon: const Icon(Icons.person_add),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context)=> login_screen()));
+              },
             ),
+          ],
         ),
-     ),
+        body: new Container(
+          //decoration: new BoxDecoration(color: Colors.black),
+        ),
+      ),
     );
   }
 }
 
 //widget that holds the image and returns it in a container
-class add_icon_asset extends StatelessWidget {
+class admin_icon_asset extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //creating an object that passes that image/logo as a parameter
 
-    AssetImage assetImage = AssetImage("images/add_icon.png");
     //create an image object from the assetimage object and pass it as parameter
-    //resize the image with set heights and width
+    AssetImage assetImage = AssetImage("images/add_icon.png");
 
-    Image add_icon_image = Image(
+    Image admin_icon_image = Image(
       image: assetImage,
       width: 70.0,
       height: 70.0,
@@ -72,7 +55,7 @@ class add_icon_asset extends StatelessWidget {
     //the method will return the logo image as a container
     // with in it will be a child that will hold the image
     return Container(
-      child: add_icon_image,
+      child: admin_icon_image,
     );
   }
 }

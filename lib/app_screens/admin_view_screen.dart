f693444483b0740dbf4d5f52a73ba38741b115da
Siddharth Notani <36894543+snotani@users.noreply.github.com>
2019-02-15 12:55:58 +0000

    Update admin_view_screen.dart

--- lib/app_screens/admin_view_screen.dart
+++ lib/app_screens/admin_view_screen.dart
@@ -81,3 +81,72 @@ class _admin_view_screenState extends State<admin_view_screen> {
     );
   }
 }
+
+Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
+
+  int quantity_check = 0;
+
+  return ListTile(
+    title: Row(
+      children: [
+        Expanded(
+          child: Text(
+            document['Item name'],
+            style: Theme.of(context).textTheme.headline,
+          ),
+        ),
+        IconButton(
+          icon: Icon(Icons.remove_circle),
+          iconSize: 50.0,
+          color: Colors.blue[700],
+          onPressed: () {
+            // remove one from quantity
+            if(document['Quantity'] > 1) {
+              quantity_check--;
+              print(quantity_check);
+              Firestore.instance.runTransaction((transaction) async {
+                DocumentSnapshot freshSnap =
+                await transaction.get(document.reference);
+                await transaction.update(freshSnap.reference, {
+                  'Quantity': freshSnap['Quantity'] - 1,
+                });
+              });
+            }
+            // Add if statement to check if quantity reaches 0 and then pop up "Are you sure message" to delete the field
+            if (document['Quantity'] == 1){
+
+              //delete_field_alert(context);
+            }
+          },
+        ),
+        Container(
+          decoration: const BoxDecoration(
+            color: Color(0xffddddff),
+          ),
+          padding: const EdgeInsets.all(10.0),
+          margin: const EdgeInsets.only(top: 15.0),
+          child: Text(
+            document['Quantity'].toString(),
+            style: Theme.of(context).textTheme.display1,
+          ),
+        ),
+        IconButton(
+          icon: Icon(Icons.add_circle),
+          color: Colors.blue[700],
+          iconSize: 50.0,
+          //onPressed: () {increase_quantity(document, quantity_check); },
+        ),
+      ],
+    ),
+    leading: new Icon(
+      Icons.fastfood,
+      size: 40.0,
+      color: Colors.blue[700],
+    ),
+    subtitle: new Text(
+      document['Donator'],
+      style: Theme.of(context).textTheme.subhead,
+    ),
+  );
+}
+

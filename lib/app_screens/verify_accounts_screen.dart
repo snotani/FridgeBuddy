1457f885db23ed3ec855edf63b67f19c2e52b688
ATCostello <37341132+ATCostello@users.noreply.github.com>
2019-03-04 00:16:46 +0000

    Added three roles, plus role change rules.
    Admins may change other admins and promote others to admin,
    Volunteers may only change users to volunteers.

--- lib/app_screens/verify_accounts_screen.dart
+++ lib/app_screens/verify_accounts_screen.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 import 'admin_home_screen.dart';
-import 'admin_update_screen.dart';
-import 'add_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'admin_home_screen.dart';
+
+final FirebaseAuth auth = FirebaseAuth.instance;
+String currentUserRole;
+
+//Get current user's role
+Future<FirebaseUser> getCurrentUser() async {
+  final FirebaseUser currentUser = await auth.currentUser();
+  Firestore.instance.collection('/users').where('uid', isEqualTo: currentUser.uid).getDocuments().then((docs){
+    if(docs.documents[0].exists) {
+      currentUserRole = docs.documents[0].data['role'].toString();
+      print(currentUserRole);
+      return currentUserRole;
+    }
+  });
+}
 
 class verify_accounts_screen extends StatefulWidget {
   @override
@@ -15,7 +30,7 @@ class _verify_accounts_screenState extends State<verify_accounts_screen> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(70.0), // here the desired height
+        preferredSize: Size.fromHeight(70.0),
         child: new AppBar(
           title: new Text("Verify Users", style: TextStyle(fontSize: 40.0)),
           centerTitle: true,
@@ -30,16 +45,18 @@ class _verify_accounts_screenState extends State<verify_accounts_screen> {
         ),
       ),
 
+      //Stream a list of each user present in the "users" collection
       body: StreamBuilder(
           stream: Firestore.instance.collection('users').snapshots(),
           builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
             if (!snapshot.hasData) return new Text('Loading...');
-
+            //Separate results
             return ListView.separated(
               separatorBuilder: (context, index) => Divider(
                 color: Colors.black,
               ),
               itemCount: snapshot.data.documents.length,
+              //For each document, build a new list item
               itemBuilder: (context, index) =>
                   _buildListItem(context, snapshot.data.documents[index], false),
             );
@@ -73,28 +90,173 @@ class _verify_accounts_screenState extends State<verify_accounts_screen> {
         iconSize: 50.0,
         color: Colors.green[700],
         onPressed: () {
+          //If user to change is admin, only another admin can change their role.
           if(document['role'] == "admin"){
-            Firestore.instance
-                .runTransaction((transaction) async {
-              await transaction.update(
-                  Firestore.instance
-                      .collection("users")
-                      .document(document['uid']),
-                  {
-                    'role': "user",
-                  });
-            });
+            //If not an admin, cannot change role
+            if(currentUserRole != "admin"){
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: new Text("Not an admin"),
+                    content: new Text("You cannot change the status of other admins."),
+                    actions: <Widget>[
+                      // Close button on alert box
+                      new FlatButton(
+                        child: new Text("OK"),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            } //If admin, can change role
+            else {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: new Text("Confirm role change"),
+                      content: new Text(
+                          "Change " + document['email'].toString() +
+                              " to user?"),
+                      actions: <Widget>[
+                        new FlatButton(
+                          child: new Text("Confirm"),
+                          onPressed: () {
+                            Firestore.instance
+                                .runTransaction((transaction) async {
+                              await transaction.update(
+                                  Firestore.instance
+                                      .collection("users")
+                                      .document(document['uid']),
+                                  {
+                                    'role': "user",
+                                  });
+                            });
+                            Navigator.pop(context);
+                          },
+                        ),
+                        new FlatButton(
+                          child: new Text("Cancel"),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    );
+                  }
+              );
+            }
+          } //If user to change is a volunteer, admins may promote to admin, else change to user
+          else if(document['role'] == "volunteer"){
+            if(currentUserRole != "admin"){
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: new Text("Confirm role change"),
+                      content: new Text(
+                          "Change " + document['email'].toString() + " to user?"),
+                      actions: <Widget>[
+                        new FlatButton(
+                          child: new Text("Confirm"),
+                          onPressed: () {
+                            Firestore.instance
+                                .runTransaction((transaction) async {
+                              await transaction.update(
+                                  Firestore.instance
+                                      .collection("users")
+                                      .document(document['uid']),
+                                  {
+                                    'role': "user",
+                                  });
+                            });
+                            Navigator.pop(context);
+                          },
+                        ),
+                        new FlatButton(
+                          child: new Text("Cancel"),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    );
+                  }
+              );
+            } else {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: new Text("Confirm role change"),
+                      content: new Text(
+                          "Change " + document['email'].toString() + " to admin?"),
+                      actions: <Widget>[
+                        new FlatButton(
+                          child: new Text("Confirm"),
+                          onPressed: () {
+                            Firestore.instance
+                                .runTransaction((transaction) async {
+                                  await transaction.update(
+                                      Firestore.instance
+                                          .collection("users")
+                                          .document(document['uid']),
+                                      {
+                            'role': "admin",
+                          });
+                    });
+                    Navigator.pop(context);
+                  },
+                ),
+                new FlatButton(
+                  child: new Text("Cancel"),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            );
+          }
+      );
+    }
           } else if(document['role'] == "user"){
-            Firestore.instance
-                .runTransaction((transaction) async {
-              await transaction.update(
-                  Firestore.instance
-                      .collection("users")
-                      .document(document['uid']),
-                  {
-                    'role': "admin",
-                  });
-            });
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: new Text("Confirm role change"),
+                    content: new Text("Change " + document['email'].toString() + " to volunteer?"),
+                    actions: <Widget>[
+                      new FlatButton(
+                        child: new Text("Confirm"),
+                        onPressed: () {
+                          Firestore.instance
+                              .runTransaction((transaction) async {
+                            await transaction.update(
+                                Firestore.instance
+                                    .collection("users")
+                                    .document(document['uid']),
+                                {
+                                  'role': "volunteer",
+                                });
+                          });
+                          Navigator.pop(context);
+                        },
+                      ),
+                      new FlatButton(
+                        child: new Text("Cancel"),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ],
+                  );
+                }
+            );
           }
         }),
       subtitle: new Text(
@@ -103,10 +265,6 @@ class _verify_accounts_screenState extends State<verify_accounts_screen> {
       ),
     );
 
-
-
   }
-
-
 }
 

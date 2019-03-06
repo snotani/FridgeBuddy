032bec2d004c03032f9a3ee4edbf5aef7172c4a1
Siddharth Notani <36894543+snotani@users.noreply.github.com>
2019-03-06 20:33:54 +0000

    Update user_update_screen.dart

--- lib/app_screens/user_update_screen.dart
+++ lib/app_screens/user_update_screen.dart
@@ -9,10 +9,16 @@ class user_update_screen extends StatefulWidget {
 }
 
 class _user_update_screen extends State<user_update_screen> {
-  @override
-  void initState(){
-    super.initState();
 
+  int quantity_count = 0;
+  Map<String,int> localCount = new Map();
+  Map<String,int> eachItem = new Map();
+  Map<String,bool> itemBool = new Map();
+  Map<String,Color> buttonColour = new Map();
+
+  @override
+  void initState() {
+    super.initState();
 
   }
 
@@ -103,6 +109,15 @@ class _user_update_screen extends State<user_update_screen> {
                 builder: (BuildContext context,
                     AsyncSnapshot<QuerySnapshot> snapshot) {
                   if (!snapshot.hasData) return new Text('Loading...');
+
+                  for(int i=0;i<snapshot.data.documents.length;i++)
+                  {
+                    localCount.putIfAbsent(snapshot.data.documents[i]['Item name'], () => snapshot.data.documents[i]['Quantity']);
+                    eachItem.putIfAbsent(snapshot.data.documents[i]['Item name'], () => 0);
+                    itemBool.putIfAbsent(snapshot.data.documents[i]['Item name'], () => true);
+                    buttonColour.putIfAbsent(snapshot.data.documents[i]['Item name'], () => Colors.grey[600]);
+                  }
+
                   return ListView.separated(
                     separatorBuilder: (context, index) =>
                         Divider(
@@ -115,19 +130,14 @@ class _user_update_screen extends State<user_update_screen> {
                   );
                 }),
           ),
+          Button_confirm(),
         ],
       ),
     );
   }
 
-
   Widget _buildListItem(BuildContext context, DocumentSnapshot document, docID) {
-    bool _isButtonDisabled = true;
-    int local_quantity = 0;
-    int quantity_count = 0;
-    local_quantity = document['Quantity'];
-    quantity_count = document['Quantity'] - local_quantity;
-    Color disabledButton = Colors.grey[600];
+    quantity_count = localCount[document['Item name']] - eachItem[document['Item name']];;
 
     return ListTile(
       title: Row(
@@ -150,19 +160,18 @@ class _user_update_screen extends State<user_update_screen> {
             color: Colors.blue[700],
             onPressed: () {
               // remove one from local quantity
-              //local_quantity--;
               setState(() {
-                local_quantity--;
+                eachItem[document['Item name']]++;
+                if (eachItem[document['Item name']] > 0) {
+                  itemBool[document['Item name']] = false;
+                  buttonColour[document['Item name']] = Colors.blue[700];
+                }
               });
-              print(document['Quantity']);
-              print(local_quantity);
-              quantity_count = document['Quantity'] - local_quantity;
-              print(quantity_count);
             },
           ),
           Container(
             child: Text(
-              '$local_quantity',
+              '$quantity_count',
               style: TextStyle(fontSize: MediaQuery
                   .of(context)
                   .size
@@ -171,22 +180,20 @@ class _user_update_screen extends State<user_update_screen> {
           ),
           IconButton(
             icon: Icon(Icons.add_circle),
-            color: disabledButton,
+            color: buttonColour[document['Item name']],
             iconSize: MediaQuery
                 .of(context)
                 .size
                 .width / 10,
-            //disabledColor: Colors.grey[600],
-            onPressed: () {
-              if (quantity_count > 0) {
-                local_quantity++;
-                print(local_quantity);
-              }
-              else {
-                setState(() {
-                  disabledButton = Colors.blue[700];
-                });
-              }
+            disabledColor: Colors.grey[600],
+            onPressed: itemBool[document['Item name']] ? null : () {
+              setState(() {
+                eachItem[document['Item name']]--;
+                if (eachItem[document['Item name']] == 0) {
+                  itemBool[document['Item name']] = true;
+                  buttonColour[document['Item name']] = Colors.grey[600];
+                }
+              });
             },
           ),
         ],
@@ -208,42 +215,44 @@ class _user_update_screen extends State<user_update_screen> {
       ),
     );
   }
-}
 
-class Button_confirm extends StatelessWidget {
-  const Button_confirm({DocumentSnapshot this.document, int this.quantity});
-
-  final DocumentSnapshot document;
-  final int quantity;
-
-  @override
-  Widget build(BuildContext context) {
+  Widget Button_confirm() {
     return Padding(
-      padding: EdgeInsets.only(bottom: MediaQuery.of(context).size.height/23.5),
+      padding: EdgeInsets.only(bottom: MediaQuery
+          .of(context)
+          .size
+          .height / 23.5),
       child: Container(
-        width: MediaQuery.of(context).size.width/2.45,
-        height: MediaQuery.of(context).size.height/13,
+        width: MediaQuery
+            .of(context)
+            .size
+            .width / 2.45,
+        height: MediaQuery
+            .of(context)
+            .size
+            .height / 13,
         child: OutlineButton(
           color: Colors.green,
           child: Text(
             "Confirm",
             textDirection: TextDirection.ltr,
-            style: TextStyle(color: Colors.green, fontSize: MediaQuery.of(context).size.width / 16.5),
+            style: TextStyle(color: Colors.green, fontSize: MediaQuery
+                .of(context)
+                .size
+                .width / 16.5),
           ),
           onPressed: () {
-            confirm_quantity_changes(this.document, this.quantity);
+            //confirm_quantity_changes(document, quantity);
             confirmDialog(context);
           },
           shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(15.0)),
-          borderSide: BorderSide(color: Colors.green,width: 5.0),),
+          borderSide: BorderSide(color: Colors.green, width: 5.0),),
       ),
     );
   }
-}
 
-void confirm_quantity_changes (DocumentSnapshot document, quantity) {
-
+  void confirm_quantity_changes(DocumentSnapshot document, quantity) {
     Firestore.instance.runTransaction((transaction) async {
       DocumentSnapshot freshSnap =
       await transaction.get(document.reference);
@@ -252,117 +261,119 @@ void confirm_quantity_changes (DocumentSnapshot document, quantity) {
       });
     });
 
-  if (quantity == 1){
-    // DELETES THE FIELD INSIDE THIS FUNCTION
-    //hide_field_alert(context, docID);
+    initState();
+    if (quantity == 1) {
+      // DELETES THE FIELD INSIDE THIS FUNCTION
+      //hide_field_alert(context, docID);
+    }
   }
-}
 
-void confirmDialog(BuildContext context) {
-  var alertDialog = AlertDialog(
-    title: new Text("Successfull!"),
-    content: new Text("You have successfully updated the fridge!"),
-  );
+  void confirmDialog(BuildContext context) {
+    var alertDialog = AlertDialog(
+      title: new Text("Successfull!"),
+      content: new Text("You have successfully updated the fridge!"),
+    );
 
-  showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return alertDialog;
-      });
-}
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alertDialog;
+        });
+  }
 
-void feedbackDialog(BuildContext context) {
-  var alertDialog = AlertDialog(
-    title: new Text("Feedback"),
-    content: new Column(
-      children: <Widget>[
-        TextField(
-          decoration: InputDecoration(hintText: 'Enter feedback here...'),
-          onChanged: (value) {
+  void feedbackDialog(BuildContext context) {
+    var alertDialog = AlertDialog(
+      title: new Text("Feedback"),
+      content: new Column(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(hintText: 'Enter feedback here...'),
+            onChanged: (value) {},
+          ),
+          SizedBox(height: 10.0,)
+        ],
+      ),
+
+    );
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alertDialog;
+        });
+  }
+
+  void statisticsDialog(BuildContext context) {
+    var alertDialog = AlertDialog(
+      title: new Text("Statistics"),
+      content: new Text(
+          "Add statistics page here - Pull data of analytics from Firebase"),
+    );
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alertDialog;
+        });
+  }
+
+  void helpDialog(BuildContext context) {
+    var alertDialog = AlertDialog(
+      title: Text("Help"),
+      content: Text(
+          "FridgeBuddy is a project where we aim to reduce food wastage in the university. "
+              "The project would do this by helping both staff and students monitor the contents of the Community Fridge. "
+              "This will also include statistics about the Community Fridge usage to reduce even more food waste within the University.",
+          style: TextStyle(height: 2.0)),
+    );
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alertDialog;
+        });
+  }
+
+  void hide_field_alert(BuildContext context, docID) {
+    var alertDialog = AlertDialog(
+      title: Text("Delete Item"),
+      content: Text("Do you wish to retrieve this last item?"),
+      actions: <Widget>[
+        FlatButton(child: Text("Yes"),
+          onPressed: () {
+            //delete_field(docID);
+            Navigator.pop(context);
           },
         ),
-        SizedBox(height: 10.0,)
+        FlatButton(child: Text("No"),
+          onPressed: () {
+            stay_on_page(context);
+          },
+        )
       ],
-    ),
+    );
 
-  );
-
-  showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return alertDialog;
-      });
-}
-
-void statisticsDialog(BuildContext context) {
-  var alertDialog = AlertDialog(
-    title: new Text("Statistics"),
-    content: new Text("Add statistics page here - Pull data of analytics from Firebase"),
-  );
-
-  showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return alertDialog;
-      });
-}
-
-void helpDialog(BuildContext context) {
-  var alertDialog = AlertDialog(
-    title: Text("Help"),
-    content: Text("FridgeBuddy is a project where we aim to reduce food wastage in the university. "
-        "The project would do this by helping both staff and students monitor the contents of the Community Fridge. "
-        "This will also include statistics about the Community Fridge usage to reduce even more food waste within the University.",
-        style: TextStyle(height: 2.0)),
-  );
-
-  showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return alertDialog;
-      });
-}
-
-void hide_field_alert (BuildContext context, docID) {
-  var alertDialog = AlertDialog(
-    title: Text("Delete Item"),
-    content: Text("Do you wish to retrieve this last item?"),
-    actions: <Widget>[
-      FlatButton( child: Text("Yes"),
-        onPressed: (){
-          //delete_field(docID);
-          Navigator.pop(context);
-        },
-      ),
-      FlatButton(child: Text("No"),
-        onPressed: (){
-          stay_on_page(context);
-        },
-      )
-    ],
-  );
-
-  showDialog(
-      context: context,
-      builder: (BuildContext context){
-        return alertDialog;
-
-      }
-  );
-}
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alertDialog;
+        }
+    );
+  }
 
 //This will delete the item field requested
-void delete_field(docID) {
-  Firestore.instance
-      .collection('Items')
-      .document(docID)
-      .delete()
-      .catchError((error){
-    print(error);
-  });
-}
+  void delete_field(docID) {
+    Firestore.instance
+        .collection('Items')
+        .document(docID)
+        .delete()
+        .catchError((error) {
+      print(error);
+    });
+  }
 
 // This will remain on the page
-void stay_on_page(BuildContext context){
-  Navigator.pop(context);
+  void stay_on_page(BuildContext context) {
+    Navigator.pop(context);
+  }
 }
